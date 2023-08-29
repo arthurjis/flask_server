3c4a249f71b7318ef43df8cbe6bab9991dcfcca0
arthurjis <jisheng@nplace.xyz>
2023-08-29 14:45:19 +0800

    Set max token

--- prompt_templates.py
+++ prompt_templates.py
@@ -7,19 +7,19 @@ prompt_templates = {
                 "template_string": """假设你是一名在中国的{{class_level}}英语老师，现有一堂{{reading_theme}}的读写整合课，请依据该文本，设计相应的作业：{{reading_article}}\n要求如下：\n1. 作业分为{{question_usage}}，各类作业分别有{{a_number}};{{b_number}};{{c_number}}道\n2. 基于英语学习活动观，从学习理解、应用实践、迁移创新三个维度进行分层设计。\n3. 下列词语为本课关键词语，在设计词语类题目时可从中进行选择：{{highlighted_words}}\n4. 均用英语回复""",
                 "model": "gpt-4",
                 "temperature": 1,
-                "max_tokens": 1024
+                "max_tokens": 2048
             },
             {
                 "template_string": """请为上述每项作业附上设计意图或参考答案。""",
                 "model": "gpt-4",
                 "temperature": 1,
-                "max_tokens": 1024
+                "max_tokens": 2048
             },
             {
                 "template_string": """请将上述题目整理成一份完整的英语题目清单，按照作业类型进行分类。并将每道题的设计意图和参考答案展示在题目后。""",
                 "model": "gpt-3.5-turbo",
                 "temperature": 1,
-                "max_tokens": 1024
+                "max_tokens": 2048
             }
         ]
     },
@@ -29,19 +29,19 @@ prompt_templates = {
                 "template_string": """假设你是一名在中国的{{class_level}}英语老师，现有一堂{{reading_theme}}的阅读课，请依据下面这篇文章，生成一篇新的延展阅读文章，并设计新文章的续写作业：\n{{reading_article}}\n要求如下：\n1. 新文章字数上限为：{{words_max}}，字数下限为：{{words_min}}\n2. 续写作业题目数为{{d_number}}道""",
                 "model": "gpt-4",
                 "temperature": 1,
-                "max_tokens": 1024
+                "max_tokens": 2048
             },
             {
                 "template_string": """请为上述续写作业附上设计意图及参考答案。""",
                 "model": "gpt-4",
                 "temperature": 1,
-                "max_tokens": 1024
+                "max_tokens": 2048
             },
             {
                 "template_string": """请将上述新阅读文章及题目、设计意图、参考答案整合和一篇完整的英语阅读材料。""",
                 "model": "gpt-3.5-turbo",
                 "temperature": 1,
-                "max_tokens": 1024
+                "max_tokens": 2048
             }
         ]
     }
@@ -78,7 +78,7 @@ def fetch_prompt_list_and_fill_placeholders_with(input_dict):
             "prompt_string": filled_template,
             "model": template_dict.get("model", "gpt-3.5-turbo"),
             "temperature": template_dict.get("temperature", 0.7),
-            "max_tokens": template_dict.get("max_tokens", 1024)
+            "max_tokens": template_dict.get("max_tokens", 2048)
         }
 
         filled_prompts.append(filled_template_dict)
